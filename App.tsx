d7b4c1a74fb8a3bc935269de434b3adbafe9747e
AndreyKapus <kapustnikov@ukr.net>
2023-07-18 15:31:53 +0300

    login

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './app/screens/login'
+import Login from './app/screens/Login';
 
 const Stack = createNativeStackNavigator()
 
@@ -10,17 +10,19 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name='Login' component={Login} options={{headerShown: false}}/>
+        <Stack.Screen name='login' component={Login}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+{/* <Stack.Screen name='Login' component={Login} options={{headerShown: false}}/> */}
--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,19 +1,66 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button, ActivityIndicator } from "react-native";
 import React, {useState} from 'react'
 import { FIREBASE_AUTH } from "../../FirebaseConfig";
-
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
     const auth = FIREBASE_AUTH;
-    
+
+    const signIn = async () => {
+        setLoading(true);
+        try{
+            const responce = await signInWithEmailAndPassword(auth, login, password);
+            console.log(responce);
+            alert('Check your email')
+        } catch (error: any) {
+            console.log(error);
+            alert('Something went wrong'+ error.message)
+        } finally {
+            setLoading(false)
+        }
+    }
+
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.container}>
+            <TextInput 
+                style={styles.input} 
+                placeholder="Email" 
+                autoCapitalize="none"
+                onChangeText={(text) => setLogin(text)}>
+            </TextInput>
+            <TextInput 
+                style={styles.input} 
+                placeholder="Password" 
+                autoCapitalize="none"
+                secureTextEntry={true}
+                onChangeText={(text) => setPassword(text)}>
+            </TextInput>
+
+            {loading ? <ActivityIndicator size="large" color="#000000"/>
+            : <Button title="Login" onPress={signIn}></Button>}
         </View>
-    )
+    );
 };
 
-export default Login
+export default Login;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginHorizontal: 20,
+        
+    },
+
+    input: {
+        marginVertical: 4,
+        height: 50,
+        borderWidth: 1,
+        borderRadius: 4,
+        color: '#000000',
+        paddingLeft: 10,
+    }
+})
