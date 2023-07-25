055fe51519ebfcb3e5a3056c878fd310ca1d5b6c
AndreyKapus <kapustnikov@ukr.net>
2023-07-25 15:41:29 +0300

    posts

--- screens/main/PostScreen.js
+++ screens/main/PostScreen.js
@@ -1,10 +1,26 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, View, StyleSheet, FlatList, Image } from "react-native";
 
 const PostScreen = ({route}) => {
-  console.log('route.params', route.params)
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    if(route.params) {
+      setPosts(prevPosts => [...prevPosts, route.params.photo])
+    }
+  }, [route.params]);
+
+  console.log(posts)
+
     return (
         <View style={styles.container}>
-            <Text>Post Screen</Text>
+            <FlatList data={posts}
+              keyExtractor={(item, idx) => idx.toString()}
+              renderItem={({item}) => (
+                <View style={styles.imageContainer}>
+                  <Image source={{uri: item}} style={styles.photo}/>
+                </View>
+            )}/>
         </View>
     )
 };
@@ -12,9 +28,20 @@ const PostScreen = ({route}) => {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: 'center',
+      marginTop: 60,
       justifyContent: 'center',
     },
+
+    photo: {
+      width: "80%", 
+      height: 200,
+    },
+
+    imageContainer: {
+      marginBottom: 20,
+      justifyContent: 'center',
+      alignItems: "center"
+    }
   
   });
 
