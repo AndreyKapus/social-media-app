0d723afa6e4ac0cb1de20e2402f8489572a5a3bc
AndreyKapus <kapustnikov@ukr.net>
2023-07-26 12:28:25 +0300

    scr

--- screens/main/PostScreen.js
+++ screens/main/PostScreen.js
@@ -1,48 +1,11 @@
-import { useEffect, useState } from "react";
-import { Text, View, StyleSheet, FlatList, Image } from "react-native";
+import { createStackNavigator } from '@react-navigation/stack';
 
-const PostScreen = ({route}) => {
-  const [posts, setPosts] = useState([]);
+const NestedScreen = createStackNavigator()
 
-  useEffect(() => {
-    if(route.params) {
-      setPosts(prevPosts => [...prevPosts, route.params.photo])
-    }
-  }, [route.params]);
-
-  console.log(posts)
-
-    return (
-        <View style={styles.container}>
-            <FlatList data={posts}
-              keyExtractor={(item, idx) => idx.toString()}
-              renderItem={({item}) => (
-                <View style={styles.imageContainer}>
-                  <Image source={{uri: item}} style={styles.photo}/>
-                </View>
-            )}/>
-        </View>
-    )
+const PostScreen = () => {
+  <NestedScreen.Navigator>
+    
+  </NestedScreen.Navigator>
 };
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: 60,
-      justifyContent: 'center',
-    },
-
-    photo: {
-      width: "80%", 
-      height: 200,
-    },
-
-    imageContainer: {
-      marginBottom: 20,
-      justifyContent: 'center',
-      alignItems: "center"
-    }
-  
-  });
 
 export default PostScreen;
---
+++ screens/nestedScreen/DefaultScreen.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { Text, View, StyleSheet, FlatList, Image } from "react-native";
+
+const DefaultScreen = ({route}) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    if(route.params) {
+      setPosts(prevPosts => [...prevPosts, route.params.photo])
+    }
+  }, [route.params]);
+
+  console.log(posts)
+
+    return (
+        <View style={styles.container}>
+            <FlatList data={posts}
+              keyExtractor={(item, idx) => idx.toString()}
+              renderItem={({item}) => (
+                <View style={styles.imageContainer}>
+                  <Image source={{uri: item}} style={styles.photo}/>
+                </View>
+            )}/>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 60,
+      justifyContent: 'center',
+    },
+
+    photo: {
+      width: "80%", 
+      height: 200,
+    },
+
+    imageContainer: {
+      marginBottom: 20,
+      justifyContent: 'center',
+      alignItems: "center"
+    }
+  
+  });
+
+export default DefaultScreen;
